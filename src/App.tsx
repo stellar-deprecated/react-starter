40b6911fe48905a659d9b276be5c4c59b7006ec4
Morley Zhi <morley@stellar.org>
2020-02-19 16:16:36 -0500

    Fix test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import { About } from "components/About";
 import { Home } from "components/Home";
 
