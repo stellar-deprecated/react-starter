13b98609f68cd1c4c81e16b7e0ca62748b780d43
Morley Zhi <morley@stellar.org>
2020-02-19 16:07:25 -0500

    Move Home components into new components/ dir, and the logo into assets/; use shared tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,8 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
-import logo from "./logo.svg";
-
-const logoSpin = keyframes`
-from {
-  transform: rotate(0deg);
-}
-to {
-  transform: rotate(360deg);
-}
-
-`;
-
-const El = styled.div`
-  text-align: center;
-`;
-
-const LogoEl = styled.img`
-  height: 40vmin;
-  pointer-events: none;
-
-  @media (prefers-reduced-motion: no-preference) {
-    animation: ${logoSpin} infinite 20s linear;
-  }
-`;
-
-const HeaderEl = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-`;
-
-const LinkEl = styled.a`
-  color: #61dafb;
-`;
+import { Home } from "components/Home";
 
 function App() {
-  return (
-    <El>
-      <HeaderEl>
-        <LogoEl src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <LinkEl
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </LinkEl>
-      </HeaderEl>
-    </El>
-  );
+  return <Home />;
 }
 
 export default App;
