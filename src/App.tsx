b09d12e8ac231470c4274424adf3026249d64154
Morley Zhi <morley@stellar.org>
2020-02-19 15:56:26 -0500

    css to styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import logo from "./logo.svg";
+
+const logoSpin = keyframes`
+from {
+  transform: rotate(0deg);
+}
+to {
+  transform: rotate(360deg);
+}
+
+`;
+
+const El = styled.div`
+  text-align: center;
+`;
+
+const LogoEl = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${logoSpin} infinite 20s linear;
+  }
+`;
+
+const HeaderEl = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const LinkEl = styled.a`
+  color: #61dafb;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <El>
+      <HeaderEl>
+        <LogoEl src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
+        <LinkEl
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </LinkEl>
+      </HeaderEl>
+    </El>
   );
 }
 
