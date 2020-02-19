8852cd2c529b72dfdf817c62cb3cd9e6ffc231d6
Morley Zhi <morley@stellar.org>
2020-02-19 16:15:06 -0500

    Add react-router and a dummy About page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { About } from "components/About";
 import { Home } from "components/Home";
 
-function App() {
-  return <Home />;
-}
+export function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
 
-export default App;
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
